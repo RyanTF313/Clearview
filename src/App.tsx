07a52e6ebb155cdf44335ed455aa5e8fc7fbbeea
RyanTF313 <r.tillman.french@gmail.com>
2025-12-29 22:04:04 -0500

    app init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      Tailwind v4 is working 🚀
+    </div>
+    </>
+  )
+}
+
+export default App
